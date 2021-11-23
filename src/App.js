f76d987a9fb47b3523ab8417b4c7171ff9b45b05
Igor Matsuoka <igor.mtka@gmail.com>
2021-11-23 15:28:20 -0300

    filtros

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from "react"
 import Home from "./components/Home"
 import Carrinho from "./components/Carrinho"
 import Produtos from "./components/Produtos"
-import Cadastro from "./components/Cadastro"
+import Cadastro from "./components/Cadastro";
+import { baseURL, headers } from './credenciais/credenciais';
+import axios from 'axios';
 
 
 export default class App extends React.Component {
 	state = {
 		paginaAtual: "Home",
+			jobs:  [],
 	  }
 	
 	  trocaPagina = () => {
@@ -15,28 +18,46 @@ export default class App extends React.Component {
 		  case ("Home"):
 			return <Home irParaProdutos={this.irParaProdutos} irParaCadastro={this.irParaCadastro}/>
 		  case ("Cadastro"):
-			return <Cadastro irParaProdutos={this.irParaProdutos}irParaHome={this.irParaHome} />
+			return <Cadastro irParaProdutos={this.irParaProdutos} irParaHome={this.irParaHome} />
 		  case ("Carrinho"):
-			return <Carrinho irParaProdutos={this.irParaProdutos}irParaHome={this.irParaHome}/>
+			return <Carrinho irParaProdutos={this.irParaProdutos} irParaHome={this.irParaHome}/>
 			case ("Produtos"):
-				return <Produtos irParaCarrinho={this.irParaCarrinho}irParaHome={this.irParaHome}/>
+				return <Produtos irParaCarrinho={this.irParaCarrinho} irParaHome={this.irParaHome} getAllJobs={this.getAllJobs} jobs={this.state.jobs}/>
 		  default:
 			return <Home/>
 		}
-	  }
+	}
 
-	  irParaHome = ()=>{
+	irParaHome = ()=>{
 		this.setState({paginaAtual:"Home"})
-	  }
-	  irParaProdutos = ()=>{
+	}
+
+	irParaProdutos = ()=>{
 		this.setState({paginaAtual:"Produtos"})
-	  }
-	  irParaCadastro = ()=>{
+	}
+
+	irParaCadastro = ()=>{
 		this.setState({paginaAtual:"Cadastro"})
-	  }
-	  irParaCarrinho = ()=>{
+	}
+
+	irParaCarrinho = ()=>{
 		this.setState({paginaAtual:"Carrinho"})
-	  }
+	}
+
+	componentDidMount = () => {
+		this.getAllJobs()
+	}
+
+	getAllJobs = async() => {
+		try {
+		  const res = await axios.get(`${baseURL}/jobs`, headers)
+		  this.setState({jobs: res.data.jobs})
+		  console.log(res.data.jobs)
+		} catch (err) {
+		  alert ("Erro!")
+		}
+	}
+
   render(){
     return (
       <div>
---
+++ src/credenciais/credenciais.js
@@ -0,0 +1,11 @@
+export const key = "71043099-2a0f-48a5-849a-7fd9adc2fdf6"
+
+export const baseURL = "https://labeninjas.herokuapp.com"
+
+//const body = "carver-labeninja-9"
+
+export const headers = {
+    headers: {
+        Authorization: key
+    }
+}
